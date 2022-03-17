7bd9134f723cc108389e7fc45d03a57e80c0cea0
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-03-17 22:23:46 +0100

    Add project files.

---
+++ SQLite_Crud_Entitiframework_NetCore/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace SQLite_Crud_Entitiframework_NetCore
+{
+    public class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = UserData.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SQLite_Crud_Entitiframework_NetCore/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SQLite_Crud_Entitiframework_NetCore
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
